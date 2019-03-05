4a76b525b806f25001e40554fed8a51a8c0d2e1c
Alex Loren <lorenalexm@gmail.com>
2019-03-04 16:11:58 -0800

    Added validation for coordinate

--- suncalc/Handler.swift
+++ suncalc/Handler.swift
@@ -1,5 +1,7 @@
 class Handler {
 	func process(with args: String) -> String {
 		return "Success"
+	func validate(latitude: Double, longitude: Double) -> Bool {
+		return (latitude < 90 && latitude > -90) && (longitude < 180 && longitude > -180)
 	}
 }
