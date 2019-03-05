4269ddef7d4109f7c25abb81967e482487c2a629
Alex Loren <lorenalexm@gmail.com>
2019-03-04 17:32:49 -0800

    Decoding coordinate args

--- suncalc/Handler.swift
+++ suncalc/Handler.swift
@@ -17,7 +17,20 @@ struct Sun: Codable {
 
 class Handler {
 	func process(with args: String) -> String {
-		return "Success"
+		guard let json = args.data(using: .utf8) else {
+			return "Unable to extract data from string"
+		}
+
+		if let coord = try? JSONDecoder().decode(Coordinate.self, from: json) {
+			if !validate(coord) {
+				return "Invalid coordinates received"
+			}
+			return "Successfully decoded coordinate"
+		} else {
+			return "Unable to decode string"
+		}
+	}
+
 	func validate(_ coord: Coordinate) -> Bool {
 		return (coord.latitude < 90 && coord.latitude > -90) && (coord.longitude < 180 && coord.longitude > -180)
 	}
